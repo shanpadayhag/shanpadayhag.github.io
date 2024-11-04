afed4b5502f304ef2e7f347397cb45c9fa137bf2
Shan Padayhag <shanpadayhag@gmail.com>
2024-11-04 18:09:19 +0800

    Resets project

--- src/configs/themes/ThemeConfig.ts
+++
@@ -1,4 +0,0 @@
-export const LIGHT_THEME = 'light theme'
-export const DARK_THEME = 'dark theme'
-
-export type THEME_TYPE = typeof LIGHT_THEME | typeof DARK_THEME
--- src/pages/work/WorkContainer.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const WorkContainer = () => (
-  <div/>
-)
-
-export default WorkContainer;
